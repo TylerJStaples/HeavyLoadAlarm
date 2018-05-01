458bd4f440713ec63e62e5b6bffbe9637fbe35b0
Tyler Staples <TylerJStaples@gmail.com>
2018-05-01 18:51:51 -0400

    UI

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
-$("#submit-button").on("click", function(){
-var studyTime = $("#study-time").val();
-console.log(studyTime);
+// Sidebar One
+
+$(document).ready(function() {
+    $('.btn').on('click', function() {
+        $('.sidebar').toggleClass('side');
+    });
+ });
+
+
+
+ // SideBar Two
+
+ // On load, it needs to be hidden
+$(document).ready( function () {
+    $('.sidebar2').hide();
 });
+
+// On click, show the sidebar
+ $(document).ready(function() {
+    $('.btn2').on('click', function() {
+        $('.sidebar2').show();
+        $('.sidebar2').toggleClass('side2');
+        
+    });
+ });
+
+ 
+ // Clock
+ function clock() {
+    setInterval (function () {
+        $('#time').html(moment().format('hh:mm:ss A'))
+    }, 1000)
+}
+clock();
