4932d5996f7d1ed0f89d40f1a6905cd47793e929
Tyler Staples <TylerJStaples@gmail.com>
2018-05-01 20:13:48 -0400

    Bootstrap 3 AND 4, logic to buttons, Timer API

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
 // Sidebar One
-
 $(document).ready(function() {
     $('.btn').on('click', function() {
         $('.sidebar').toggleClass('side');
     });
  });
-
-
-
  // SideBar Two
-
  // On load, it needs to be hidden
 $(document).ready( function () {
     $('.sidebar2').hide();
 });
-
 // On click, show the sidebar
  $(document).ready(function() {
     $('.btn2').on('click', function() {
         $('.sidebar2').show();
-        $('.sidebar2').toggleClass('side2');
-        
+        $('.sidebar2').toggleClass('side2');  
     });
  });
-
- 
  // Clock
  function clock() {
     setInterval (function () {
@@ -32,3 +23,13 @@ $(document).ready( function () {
     }, 1000)
 }
 clock();
+var timerIsDone = false;
+var start = function(){
+    alert("The timer has ended");
+    return timerIsDone = true;
+}
+$("#start").on("click", function(){
+    setTimeout(start, 3000);
+});
+//25 minutes = 1 500 000 ms
+console.log(timerIsDone);
