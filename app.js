741be7739815ba2ece15896d9c7dcc96136a9596
unknown <mjtedder@gmail.com>
2018-05-03 19:25:30 -0400

    merge fuck

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 //Initialize Firebase for adding songs to playlist for Music API to pull
 $(document).ready(function() {
 
-  var config = {
-    apiKey: "AIzaSyBLRc8mQwW2r4n8bc8U6uXl7XotQpmRy-A",
-    authDomain: "pomodoro-timer-project.firebaseapp.com",
-    databaseURL: "https://pomodoro-timer-project.firebaseio.com",
-    projectId: "pomodoro-timer-project",
-    storageBucket: "pomodoro-timer-project.appspot.com",
-    messagingSenderId: "349004114335"
-  };
-  firebase.initializeApp(config);
+ var config = {
+   apiKey: "AIzaSyBLRc8mQwW2r4n8bc8U6uXl7XotQpmRy-A",
+   authDomain: "pomodoro-timer-project.firebaseapp.com",
+   databaseURL: "https://pomodoro-timer-project.firebaseio.com",
+   projectId: "pomodoro-timer-project",
+   storageBucket: "pomodoro-timer-project.appspot.com",
+   messagingSenderId: "349004114335"
+ };
+ firebase.initializeApp(config);
 
 
 
-  // Sidebar One
-  $(document).ready(function() {
-    $('.btn').on('click', function() {
-      $('.sidebar').toggleClass('side');
+ // Sidebar One
+ $(document).ready(function() {
+   $('.btn').on('click', function() {
+     $('.sidebar').toggleClass('side');
 
       $.ajax({
         type : 'POST',
@@ -53,27 +53,27 @@ $(document).ready(function() {
       $('.sidebar2').show();
       $('.sidebar2').toggleClass('side2');
 
-      var database = firebase.database();
+     var database = firebase.database();
 
 
-    });
-    });
-  });
-  // Clock
-  function clock() {
-    setInterval(function() {
-      $('#time').html(moment().format('hh:mm:ss A'))
-    }, 1000)
-  }
-  clock();
-  var timerIsDone = false;
-  var start = function() {
-    alert("The timer has ended");
-    return timerIsDone = true;
-  }
-  $("#start").on("click", function() {
-    setTimeout(start, 3000);
-  });
-  //25 minutes = 1 500 000 ms
-  console.log(timerIsDone);
+   });
+   });
+ });
+ // Clock
+ function clock() {
+   setInterval(function() {
+     $('#time').html(moment().format('hh:mm:ss A'))
+   }, 1000)
+ }
+ clock();
+ var timerIsDone = false;
+ var start = function() {
+   alert("The timer has ended");
+   return timerIsDone = true;
+ }
+ $("#start").on("click", function() {
+   setTimeout(start, 3000);
+ });
+ //25 minutes = 1 500 000 ms
+ console.log(timerIsDone);
 });
