58586ccc4e948675bc83125185f41343c63e295a
Tyler Staples <TylerJStaples@gmail.com>
2018-05-03 21:28:32 -0400

    Napster

--- app.js
+++ app.js
@@ -11,39 +11,27 @@ $(document).ready(function() {
  };
  firebase.initializeApp(config);
 
-
-
  // Sidebar One
  $(document).ready(function() {
-   $('.btn').on('click', function() {
-     $('.sidebar').toggleClass('side');
-
-     $.ajax({
-       type : 'POST',
-       url : 'https://ws.audioscrobbler.com/2.0/',
-       data : 'method=artist.getinfo&' +
-              'playlist.fetch&' +
-              'artist=Nas&' +
-              'api_key=4cfb3ad46313f41be3ff0885b455659a&' +
-              'format=json',
-       method : 'package.method',
-       dataType : 'jsonp',
-       success : function(data) {
-           console.log(data);
-       },
-       error : function(code, message){
-           // Handle error here
-       }
-   })
-   .then(function(response) {
-     var results = response.data;
-     var p = $("<p>").text(results);
-     $("#lastfm").append(p);
-     var imglastfm = $("<img>");
-     imglastfm.attr("src =" + results.artist.image[0].text);
-
-   });
- });
+    $('.btn').on('click', function() {
+      $('.sidebar').toggleClass('side');
+      var queryURL = "https://api.napster.com/v2.1/tracks/top?apikey=MWEyNTMyOGEtNTZiYS00ZjNmLThkMGMtMzExMTllNjQzMGJh";
+ 
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      .then(function(response) {
+        console.log(response);
+       var ul = $("<ul>");
+       $("#napsteristhefuckingshit").append(ul);
+        var results = response.tracks;
+        for (var i = 0; i < response.tracks.length; i++) {
+          var list = $("<li>");
+          ul.append(list);
+          list.append(results[i].artistName + ", " + results[i].name + ", " + results[i].previewURL);
+        }
+      })
  // SideBar Two
  // On load, it needs to be hidden
  $(document).ready(function() {
@@ -57,7 +45,6 @@ $(document).ready(function() {
 
      var database = firebase.database();
 
-
    });
    });
  });
@@ -70,12 +57,27 @@ $(document).ready(function() {
  clock();
  var timerIsDone = false;
  var start = function() {
-   alert("The timer has ended");
-   return timerIsDone = true;
+   timerIsDone = true;
+   console.log(timerIsDone);
+    //pause playing
+    $(".audioDemo").trigger('pause');
+    //set play time to 0
+    $(".audioDemo").prop("currentTime",0);
  }
+ timerIsDone = false;
  $("#start").on("click", function() {
-   setTimeout(start, 3000);
+    setTimeout(start, 3000);
+    //setTimeout(start, 1500000);
+    $(".lofi").trigger('play');
  });
  //25 minutes = 1 500 000 ms
- console.log(timerIsDone);
+ $("#stop").on("click", function() {
+    $(".lofi").trigger('pause');
+ });
+ $("#reset").on("click", function() {
+    //pause playing
+    $(".audioDemo").trigger('pause');
+    //set play time to 0
+    $(".audioDemo").prop("currentTime",0);
+ });
 });
